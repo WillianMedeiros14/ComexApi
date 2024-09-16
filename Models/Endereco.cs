38d6894873187487282d0054bbbb1d82668f9efa
Willian Medeiros <willianmedeiroslm@gmail.com>
2024-09-16 09:18:33 -0400

    fature: create class Cliente and Endereco

---
+++ Models/Endereco.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+namespace ComexAPI.Models;
+
+public class Endereco
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "O campo Bairro é obrigatário.")]
+    public string Bairro { get; set; }
+
+    [Required(ErrorMessage = "O campo Cidade é obrigatário.")]
+    public string Cidade { get; set; }
+
+    public string Complemento { get; set; }
+
+    [Required(ErrorMessage = "O campo Estado é obrigatário.")]
+    public string Estado  { get; set; }
+
+
+    [Required(ErrorMessage = "O campo Rua é obrigatário.")]
+    public string Rua  { get; set; }
+
+    [Required(ErrorMessage = "O campo Número é obrigatário.")]
+    public int Numero { get; set; }
+}
