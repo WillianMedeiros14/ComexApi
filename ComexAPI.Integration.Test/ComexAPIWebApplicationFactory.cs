d6e15aaec0a3ae9ac0ff3cc76ae38f43fb1d1f2f
Willian Medeiros <willianmedeiroslm@gmail.com>
2024-09-18 18:31:59 -0300

    test and buid: refactor run test'

--- ComexAPI.Integration.Test/ComexAPIWebApplicationFactory.cs
+++ ComexAPI.Integration.Test/ComexAPIWebApplicationFactory.cs
@@ -7,12 +7,28 @@ using System;
 using System.Linq;
 
 using ComexAPI.Data;
+using Testcontainers.PostgreSql;
 
 namespace ComexAPI.Integration.Test
 {
-    public class ComexAPIWebApplicationFactory : WebApplicationFactory<Program>
+    public class ComexAPIWebApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
     {
         public ProdutoContext Context { get; set; }
+
+        private IServiceScope scope;
+
+        private readonly PostgreSqlContainer _postgresqlContainer;
+
+        public ComexAPIWebApplicationFactory()
+        {
+            // Configura o container PostgreSQL para os testes
+            _postgresqlContainer = new PostgreSqlBuilder()
+                .WithDatabase("produtos_test")
+                .WithUsername("postgres")
+                .WithPassword("root")
+                .WithImage("postgres:16") // Define a imagem como postgres:16
+                .Build();
+        }
 
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
@@ -20,8 +36,7 @@ namespace ComexAPI.Integration.Test
             {
                 services.RemoveAll(typeof(DbContextOptions<ProdutoContext>));
 
-                var connectionString = Environment.GetEnvironmentVariable("DATABASE_URL_TEST")
-                     ?? "Server=localhost;Port=5433;Database=produtos_test;Username=postgres;Password=Bemol@99";
+                var connectionString = _postgresqlContainer.GetConnectionString();
 
                 services.AddDbContext<ProdutoContext>(options =>
                     options.UseLazyLoadingProxies()
@@ -29,16 +44,30 @@ namespace ComexAPI.Integration.Test
 
                 var serviceProvider = services.BuildServiceProvider();
 
-                using (var scope = serviceProvider.CreateScope())
-                {
-                    var scopedServices = scope.ServiceProvider;
-                    Context = scopedServices.GetRequiredService<ProdutoContext>();
+                //using (var scope = serviceProvider.CreateScope())
+                //{
+                //    var scopedServices = scope.ServiceProvider;
+                //    Context = scopedServices.GetRequiredService<ProdutoContext>();
 
-                    Context.Database.Migrate();
-                }
+                //    Context.Database.Migrate();
+                //}
             });
 
             base.ConfigureWebHost(builder);
+        }
+
+        public async Task InitializeAsync()
+        {
+            await _postgresqlContainer.StartAsync();
+            this.scope = Services.CreateScope();
+            Context = scope.ServiceProvider.GetRequiredService<ProdutoContext>();
+            Context.Database.Migrate();
+
+        }
+
+        async Task IAsyncLifetime.DisposeAsync()
+        {
+            await _postgresqlContainer.DisposeAsync();
         }
     }
 }
